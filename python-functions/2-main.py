083a7c2c3abc6e377344c238bfffd5d89220964d
ehoneahobed <ehoneahobed@hotmail.com>
2023-07-12 07:54:05 -0400

    Add solution to task 2

---
+++ python-functions/2-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+convert_to_celsius = __import__('2-temperature').convert_to_celsius
+
+print(convert_to_celsius(50))
+print(convert_to_celsius(-0))
+print(convert_to_celsius(-425))
+print(convert_to_celsius(23))
---
+++ python-functions/2-temperature.py
@@ -0,0 +1,8 @@
+"""
+Module to convert temperature
+"""
+
+def convert_to_celsius(fahrenheit):
+    """Returns the converted value"""
+    celsius = (fahrenheit - 32) * 5 / 9
+    return celsius
