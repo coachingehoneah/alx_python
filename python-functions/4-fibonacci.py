21fdbe06f0e5c4278d372d08f221592080ee7d61
ehoneahobed <ehoneahobed@hotmail.com>
2023-07-13 18:53:37 -0400

    Add solution to task 4

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci_sequence(n):
+    sequence = []
+    
+    if n >= 1:
+        sequence.append(0)
+    
+    if n >= 2:
+        sequence.append(1)
+    
+    for i in range(2, n):
+        next_number = sequence[i-1] + sequence[i-2]
+        sequence.append(next_number)
+    
+    return sequence
