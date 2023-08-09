0bc7119fb803eb287b8286a9f9f2c7067169e4ed
ehoneahobed <ehoneahobed@hotmail.com>
2023-08-08 22:15:48 -0400

    Add solution to task 0

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+"""
+Script that fetches a given url
+"""
+
+import requests
+
+res = requests.get("https://alu-intranet.hbtn.io/status")
+
+returned_type = type(res.text)
+content = res.text
+
+if __name__ == "__main__":
+
+    print("Body response:")
+    print("\t- type: {}".format(returned_type))
+    print("\t- content: {}".format(content))
