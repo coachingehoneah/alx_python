d90fb49c22aa1fc1c17b956a9058743825fcbc03
ehoneahobed <ehoneahobed@hotmail.com>
2023-08-10 08:49:49 -0400

    Add solution to task 1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module comments goes0
+
+This is a regular module comment
+"""
+
+import requests
+import sys
+req = requests.get(sys.argv[1])
+print(req.headers['X-Request-Id'])
+
